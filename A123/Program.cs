e60d1a4d64b81003ea9c23915d8ab17992338854
Po Lung Wu <wupojung@gmail.com>
2024-08-23 10:35:07 +0800

    第一次測試成功

---
+++ A123/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace A123
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("999");
+        }
+    }
+}
